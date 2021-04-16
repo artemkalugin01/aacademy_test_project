30a3cf27770e9a9245310eb74072c232940b50d2
aokalugin <aokalugin@edu.hse.ru>
2021-04-16 17:56:04 +0300

    total_cost unused util fix, unused imports deleted

--- endpoints/total_cost.py
+++ endpoints/total_cost.py
@@ -1,15 +1,13 @@
-import os
-
-import psycopg2
 from fastapi import APIRouter
+
+from utils.db_utils import connect_db
 
 total_cost_router = APIRouter(prefix='/total_cost', tags=['total_cost'])
 
 
 @total_cost_router.get('', tags=['total_cost'])
 async def get_total_cost():
-    con = psycopg2.connect(database=os.getenv('DATABASE'), user=os.getenv('USER'), password=os.getenv('PASSWORD'),
-                           host=os.getenv('HOST'), port=os.getenv('PORT'))
+    con = connect_db()
     cur = con.cursor()
     cur.execute("SELECT SUM(amount * price) FROM resources")
     total = cur.fetchone()[0]
